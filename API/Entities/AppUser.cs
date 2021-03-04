f8399da8da8b6c4f3cbb3d1d8235419bfc31f96a
Artur Lizakowski <arturlizakowski@gmail.com>
2021-03-04 18:52:01 +0100

    InitialCommit

---
+++ API/Entities/AppUser.cs
@@ -0,0 +1,18 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace API.Entities
+{
+    public class AppUser
+    {
+      [Key]
+        public int Id { get; set; }
+
+        public string UserName { get; set; }
+
+        public string DisplayName { get; set; }
+
+        public byte[] PasswordHash { get; set; }
+
+        public byte[] PasswordSalt { get; set; }
+    }
+}
